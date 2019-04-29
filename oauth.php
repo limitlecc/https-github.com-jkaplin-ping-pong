8d97c0fd4ff869138cf62dec2ec5d63e579c72d3
Joseph Kaplin <kaplinjoseph@gmail.com>
2019-04-29 08:04:19 -0700

    Update

--- oauth.php
+++ oauth.php
@@ -2,18 +2,11 @@
 	echo $_GET["code"];
 	$code = $_GET["code"];
 
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_URL, 'https://api.intra.42.fr/oauth/token');
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=511d20aaebf083f243087bc461886b5d79bd862e029d400e9870fa7d1982c6b6&redirect_uri=https://jkaplin.github.io/ping-pong/tournament&grant_type=authorization_code&code=$code");
-
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-	$result = curl_exec($ch);
-	if (curl_errno($ch)) {
-			echo 'Error:' . curl_error($ch);
-	}
-
-	var_dump($result);
+	exec("curl -F grant_type=authorization_code \
+			-F client_id=511d20aaebf083f243087bc461886b5d79bd862e029d400e9870fa7d1982c6b6 \
+			-F client_secret=ee8726114499258fa121e7e0c78c643f887429b598c8ff9af5121e496ebfa2fa \
+			-F code=$code \
+			-F redirect_uri=https://ft-ping-pong.herokuapp.com/index.html \
+			-X POST https://api.intra.42.fr/oauth/token",$o);
+	print_r($o);
 ?>
