8e2a977cb645931e66f1dfac7efe86387f94f76e
Joseph Kaplin <kaplinjoseph@gmail.com>
2019-04-29 09:31:30 -0700

    P

--- tournament/index.php
+++ tournament/index.php
@@ -1,4 +1,27 @@
 <?php
 	session_start();
-	var_dump($_SESSION);
+	if (isset($_SESSION["user"]))
+	{
+		$user = $_SESSION['user'];
+		$intra = $user->login;
+		$name = $user->displayname;
+		$img = $user->image_url;
+	}
+	else
+	{
+		$intra = "GUEST";
+		$name = "GUEST";
+		$img = "guest.jpg";
+	}
 ?>
+<body>
+	<h1>
+		<?php
+			echo $intra;
+			echo PHP_EOL;
+			echo $name;
+			echo PHP_EOL;
+			echo $img;
+		?>
+	</h1>
+</body>
