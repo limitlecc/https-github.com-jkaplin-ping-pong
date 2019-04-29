ec3e5c4a6fd90e2f5091675ed0456d0dfcfb438c
Joseph Kaplin <kaplinjoseph@gmail.com>
2019-04-29 08:27:34 -0700

    P

--- oauth.php
+++ oauth.php
@@ -7,6 +7,9 @@
 			-F client_secret=ee8726114499258fa121e7e0c78c643f887429b598c8ff9af5121e496ebfa2fa \
 			-F code=$code \
 			-F redirect_uri=https://ft-ping-pong.herokuapp.com/oauth.php \
-			-X POST https://api.intra.42.fr/oauth/token",$o);
-	print_r($o);
+			-X POST https://api.intra.42.fr/oauth/token",$arr);
+	print_r($arr);
+	$access_token = $arr["access_token"];
+	exec("curl -H 'Authorization: Bearer $access_token' https://api.intra.42.fr/v2/me", $out);
+	print_r($arr);
 ?>
