296d5877f6c3892a3a36d2722126258867c67ae5
Camilla Bernabo <cbernabo@e1z2r9p40.42.us.org>
2019-04-29 23:38:52 -0700

    form html

--- tournament/index.php
+++ tournament/index.php
@@ -26,7 +26,7 @@
   <meta charset="utf-8">
   <title>ft_ping-pong</title>
 	<link rel="shortcut icon" href="ping-pong.ico" />
-  <link rel="stylesheet" href="tournament/style.css" />
+  <link rel="stylesheet" href="style.css" />
 	<script type="text/javascript" src="tournament/script.js"></script>
 </head>
 
@@ -36,6 +36,27 @@
 			echo "Hi there $name";
 		?>
 	</h1>
+	<div class="container">  
+    	<form id="form" action="" method="post">
+			<h3>Submit the Match Score</h3>
+			<h4>Each player has to submit one form</h4>
+			<fieldset>
+				<input placeholder="Your Intra" type="text" required autofocus>
+			</fieldset>
+			<fieldset>
+				<input placeholder="Your Score" type="number" min="0" max="21" required>
+			</fieldset>
+			<fieldset>
+				<input placeholder="Opponent Intra" type="text" required>
+			</fieldset>
+			<fieldset>
+				<input placeholder="Opponent Score" type="number" min="0" max="21" required>
+			</fieldset>
+			<fieldset>
+				<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+			</fieldset>
+    	</form>
+	</div>
 	<?php if (!$guest) { ?>
 		<h1>Join the Tournament</h1>
 	<?php } ?>
