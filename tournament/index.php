1b0d92ed12c539212f4987c7d3534a2d6f168181
jkaplin <kaplinjoseph@gmail.com>
2019-05-01 10:47:38 -0700

    P

--- tournament/index.php
+++ tournament/index.php
@@ -89,7 +89,7 @@
 		<button id="enter">Enter The Tournament</button>
 	</form>
 
-<!--	<iframe src="https://challonge.com/m5u4u1c4/module?multiplier=2" width="100%" height="70%" frameborder="0" scrolling="auto" allowtransparency="true"></iframe> -->
+	<iframe src="https://challonge.com/m5u4u1c4/module?multiplier=2" width="100%" height="70%" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
 	<div class="container">  
 		<form id="form" action="" method="post">
 			<h3>Submit the Match Score</h3>
