153eb3e9115d03a28d3efa8411b57859ef52fa61
jkaplin <kaplinjoseph@gmail.com>
2019-05-01 11:26:29 -0700

    P

--- tournament/submit.php
+++ tournament/submit.php
@@ -1,13 +1,15 @@
 
-<?php	
-	if (isset($_POST["submit"]) && $_POST["value"] === "OK")
+<?php
+	echo $_POST["submit"];
+	if (isset($_POST["submit"]) && $_POST["submit"] === "OK")
 	{
+		echo "IN";
 		$submitted = 1;
 		$intra_name = $_POST["p1"];
 		$score = $_POST["p1_score"];
 		$op_score = $_POST["p2_score"];
 		updateMatchScore($intra_name, $score, $op_score);
 	}
-	header("Location: http://ft-ping-pong.herokuapp.com/tournament/");
-	exit;
+	//header("Location: http://ft-ping-pong.herokuapp.com/tournament/");
+	//exit;
 ?>
